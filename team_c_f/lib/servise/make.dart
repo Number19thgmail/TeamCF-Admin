b647e969104a7c54cc4ca9cb8a61ec0a42f64097
Dmitry <number19th@gmail.com>
2021-04-10 21:20:08 +0300

    Создание расписания, локально

--- team_c_f/lib/servise/make.dart
+++ team_c_f/lib/servise/make.dart
@@ -35,7 +35,7 @@ List<DayView> makeDays(List<ShortMatch> data) {
 }
 
 String showPoints(int points) {
-  // Функция изменения склонения слова "Очки"
+  // Функция изменения склонения слова "N очков"
   return points.toString() +
       ' ' +
       ((points % 10 == 1 && points ~/ 10 != 1)
@@ -43,6 +43,53 @@ String showPoints(int points) {
           : ((points - 1) % 10 - 4 < 0 && points ~/ 10 != 1)
               ? 'очка'
               : 'очков');
+}
+
+String showRegisterTeam(int count) {
+  // Функция изменения склонения слова "Зарегистрировано"
+  return (count % 10 == 1 && count ~/ 10 != 1)
+      ? 'Зарегистрирована'
+      : 'Зарегистрировано';
+}
+
+String showFullTeam(int count) {
+  // Функция изменения склонения выражения "Готово к участию"
+  return (count % 10 == 1 && count ~/ 10 != 1)
+      ? 'Готова к участию'
+      : 'Готово к участию';
+}
+
+String showTeam(int points) {
+  // Функция изменения склонения слова "N команд"
+  return points.toString() +
+      ' ' +
+      ((points % 10 == 1 && points ~/ 10 != 1)
+          ? 'команда'
+          : ((points - 1) % 10 - 4 < 0 && points ~/ 10 != 1)
+              ? 'команды'
+              : 'команд');
+}
+
+String showMatches({@required int teams, @required int circles}) {
+  int matches = (teams * (teams - 1) / 2).round() * circles;
+  return matches.toString() +
+      ' ' +
+      ((matches % 10 == 1 && matches ~/ 10 != 1)
+          ? 'матч'
+          : ((matches - 1) % 10 - 4 < 0 && matches ~/ 10 != 1)
+              ? 'матча'
+              : 'матчей');
+}
+
+String showTours({@required int teams, @required int circles}) {
+  int tours = circles * (teams.isEven ? teams - 1 : teams);
+  return tours.toString() +
+      ' ' +
+      ((tours % 10 == 1 && tours ~/ 10 != 1)
+          ? 'тур'
+          : ((tours - 1) % 10 - 4 < 0 && tours ~/ 10 != 1)
+              ? 'тура'
+              : 'туров');
 }
 
 int calculatePointPerMatch({
--- team_c_f/lib/view/schedule.dart
+++ team_c_f/lib/view/schedule.dart
@@ -1,21 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:team_c_f/data/data.dart';
+import 'package:team_c_f/data/schedule.dart';
+import 'package:team_c_f/data/tournament.dart';
+import 'package:team_c_f/servise/make.dart';
 
-class ScheduleView extends StatelessWidget {
+class ScheduleView extends StatefulWidget {
   // Класс, отображающий результаты тура, по умолчанию текущего
   const ScheduleView({Key key}) : super(key: key);
 
   @override
+  _ScheduleViewState createState() => _ScheduleViewState();
+}
+
+class _ScheduleViewState extends State<ScheduleView> {
+  TextEditingController circle = TextEditingController();
+  List<Tour> timetable = [];
+
+  @override
   Widget build(BuildContext context) {
+    int registTeam = 16; //context.watch<Tournament>().allTeams.length;
+    int fullTeam = context
+        .read<Tournament>()
+        .allTeams
+        .where((team) => team.members.length == 3)
+        .length;
     return Container(
-      child: ElevatedButton(
-        child: Text('Создать 1 тур'),
-        onPressed: () {
-          context.read<Account>().changeSignIn();
-          // DatabaseService().updateCurrentTour()
-        },
-      ), //Text('Выбираем тур и показываем его результаты или календарь'),
+      child: Card(
+        child: ListTile(
+          title: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  showRegisterTeam(registTeam) + ' ' + showTeam(registTeam),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  showFullTeam(fullTeam) + ' ' + showTeam(fullTeam),
+                ),
+                SizedBox(height: 20),
+                Text('Укажите количество кругов в чемпионате'),
+                TextField(
+                  controller: circle,
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Ожидается ' +
+                      showMatches(
+                        teams: registTeam,
+                        circles: 2,
+                      ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Ожидается ' +
+                      showTours(
+                        teams: registTeam,
+                        circles: 2,
+                      ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: createTimetable,
+                  child: Text(
+                    'Создать расписание',
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ...timetable.map(
+                        (tour) {
+                          String str = '';
+                          tour.pair.forEach(
+                            (pair) {
+                              str += pair + ' \b';
+                            },
+                          );
+                          return Text(tour.tour + ' тур. Играют $str');
+                        },
+                      ).toList(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      //Text('Выбираем тур и показываем его результаты или календарь'),
     );
+  }
+
+  void createTimetable() {
+    List<String> teams = [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5', '6'
+    ]; //context.read<Tournament>().allTeamNames;
+    List<Tour> curr = [];
+    int circles = 2; //int.parse(circle.text);
+    int count = teams.length;
+    int tour = (count.isEven ? count - 1 : count);
+    for (int q = 0; q < circles; q++) {
+      for (int i = 0; i < tour; i++) {
+        List<String> pair = [];
+        for (int j = 0; j < count ~/ 2; j++) {
+          int k = count.isEven ? j : j + 1;
+          int home = i.isOdd
+              ? k == 0
+                  ? count - 1
+                  : (count ~/ 2 + i ~/ 2 + k + (count.isEven ? 0 : 1)) %
+                      (count.isOdd ? count : count - 1)
+              : k + i ~/ 2;
+          int away = i.isOdd
+              ? count ~/ 2 + i ~/ 2 - j
+              : k == 0
+                  ? count - 1
+                  : (count + i ~/ 2 - 1 - k + (count.isEven ? 0 : 1)) %
+                      (count.isOdd ? count : count - 1);
+          String element = q.isEven
+              ? teams[home] + ' - ' + teams[away]
+              : teams[away] + ' - ' + teams[home];
+          pair.add(element);
+        }
+        curr.add(Tour.basic(tour: (i + 1 + q * tour).toString(), pair: pair));
+      }
+    }
+    setState(() {
+      timetable = curr;
+    });
   }
 }
