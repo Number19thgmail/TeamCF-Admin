f16cbeb9bc498de1560df741e171c45f6b656ab7
Dmitry <number19th@gmail.com>
2021-04-12 18:23:14 +0300

    Перед выбором матча

--- team_c_f/lib/element/tour.dart
+++ team_c_f/lib/element/tour.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:team_c_f/data/schedule.dart';
 import 'package:team_c_f/data/tournament.dart';
+import 'package:team_c_f/element/meet.dart';
 import 'package:team_c_f/element/schedule.dart';
+import 'package:team_c_f/page/selectmatch.dart';
+import 'package:team_c_f/view/match.dart';
 
 class ShowTour extends StatefulWidget {
   // Класс, отображающий результаты выбранного тура тура
@@ -13,14 +17,63 @@ class ShowTour extends StatefulWidget {
 }
 
 class _ShowTourState extends State<ShowTour> {
+  Tour selectTour;
+
+  @override
+  void initState() {
+    super.initState();
+    context
+        .read<Tournament>()
+        .getTour(
+            tour: widget.tour == null
+                ? context.read<Tournament>().selectTour
+                : widget.tour)
+        .then(
+          (value) => setState(
+            () {
+              selectTour = value;
+            },
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return widget.tour == null
-        ? Text(
-            'Просмотр выбранного тура',
+    return selectTour != null
+        ? Column(
+            children: [
+              Card(
+                color: Colors.yellow,
+                child: ListTile(
+                    leading: Text(selectTour.tour),
+                    title: Text(
+                        '${selectTour.matches.where((element) => element.started).length}/10 матчей')),
+              ),
+              SizedBox(height: 20),
+              selectTour.matches.length == 10
+                  ? Wrap(
+                      spacing: 10,
+                      runSpacing: 10,
+                      children: [
+                        ...selectTour.matches
+                            .map((match) => MatchView(match: match)),
+                      ],
+                    )
+                  : ElevatedButton(
+                      onPressed: () {
+                        SelectMatch();
+                      },
+                      child: Text('Выбрать матчи')),
+              SizedBox(height: 20),
+              Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  ...selectTour.pair.map((pair) => MeetView(match: pair)),
+                ],
+              )
+            ],
           )
-        : Text(
-            'Выбран текущий тур',
-          );
+        : Text('Загрузка данных тура');
   }
 }
--- team_c_f/lib/main.dart
+++ team_c_f/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -71,94 +70,6 @@ class _PageState extends State<Page> {
           ? Homepage() // Домашняя страница приложения
           : Sign(), // Регистрация в приложении + вход в гугл
     );
-  }
-}
-
-class SelectMatch extends StatefulWidget {
-  SelectMatch({Key key}) : super(key: key);
-
-  @override
-  _SelectMatchState createState() => _SelectMatchState();
-}
-
-class _SelectMatchState extends State<SelectMatch> {
-  bool signIn = false;
-  User user;
-  List<String> range = [];
-  List<DateTime> period = [];
-
-  Future<void> download() async {
-    range.forEach(
-      (date) {
-        http.Client()
-            .get(Uri.parse('https://www.sports.ru/football/match/$date/'))
-            .then((value) {
-          context.read<DataShortMatch>().addData([
-            ...getMatchs(
-              body: value.body,
-              date: '$date',
-            )
-          ]);
-        });
-      },
-    );
-  }
-
-  Widget home(int count) {
-    return Container(
-      child: ListView(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              range.clear();
-              DateRangePicker.showDatePicker(
-                context: context,
-                initialFirstDate: DateTime.now(),
-                initialLastDate: DateTime.now(),
-                firstDate: DateTime(2021),
-                lastDate: DateTime(2040),
-              ).then((value) {
-                setState(() {
-                  period = value;
-                  for (DateTime i = value.first;
-                      i.isBefore(value.last.add(Duration(minutes: 1)));
-                      i = i.add(Duration(days: 1))) {
-                    range.add(DateFormat('yyyy-MM-dd').format(i));
-                  }
-                });
-                context.read<DataShortMatch>().clearData();
-                download();
-              });
-            },
-            child: Text('Выбрать даты'),
-          ),
-          Text(
-            '$count/10',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: count > 10 ? Colors.red : Colors.green,
-            ),
-          ),
-          ...makeDays(context.watch<DataShortMatch>().getData),
-          ElevatedButton(
-              onPressed: () {
-                signOutGoogle().then(
-                  (value) => setState(() {
-                    signIn = false;
-                  }),
-                );
-              },
-              child: Text('logout'))
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return signIn
-        ? home(context.watch<DataShortMatch>().countSelected)
-        : Sign();
   }
 }
 
